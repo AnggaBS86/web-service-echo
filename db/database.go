60415cc4875412150d416d6732b703c17995dd52
Angga Bayu <anggabs86@gmail.com>
2024-09-11 18:42:57 +0700

    add mutext for prevent race condition in DB

--- db/database.go
+++ db/database.go
@@ -4,28 +4,39 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 	"web-service-echo/config"
 
 	"github.com/jinzhu/gorm"
 )
 
-var db *gorm.DB
-var err error
+var (
+	db  *gorm.DB
+	err error
+	mu  sync.Mutex // Mutex to prevent race conditions
+)
 
+// Init initializes the database connection with a mutex to prevent race conditions.
 func Init() {
+	mu.Lock() // Lock the mutex to ensure no race conditions
+	defer mu.Unlock()
+
+	// Initialize the database
 	db, err = gorm.Open(os.Getenv("DB_FACTORY"), config.GetDialect())
 	if err != nil {
 		panic("Error connection : " + err.Error())
 	}
 
+	// Setup the connection pool
 	setupConnectionPool(db)
 }
 
+// setupConnectionPool configures the connection pool settings for the database.
 func setupConnectionPool(db *gorm.DB) {
-	maxIddleCon, err := strconv.Atoi(os.Getenv("DB_MAX_IDDLE_CON"))
+	maxIdleCon, err := strconv.Atoi(os.Getenv("DB_MAX_IDDLE_CON"))
 	if err != nil {
-		maxIddleCon = 100
+		maxIdleCon = 100
 		log.Println(err.Error())
 	}
 	maxOpenCon, err := strconv.Atoi(os.Getenv("DB_MAX_OPEN_CON"))
@@ -34,11 +45,15 @@ func setupConnectionPool(db *gorm.DB) {
 		log.Println(err.Error())
 	}
 
-	db.DB().SetMaxIdleConns(maxIddleCon)
+	db.DB().SetMaxIdleConns(maxIdleCon)
 	db.DB().SetMaxOpenConns(maxOpenCon)
 	db.DB().SetConnMaxLifetime(time.Hour)
 }
 
+// GetDB provides thread-safe access to the db instance.
 func GetDB() *gorm.DB {
+	mu.Lock() // Lock the mutex to ensure thread safety
+	defer mu.Unlock()
+
 	return db
 }
